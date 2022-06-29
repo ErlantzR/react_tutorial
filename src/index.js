e383060f5d5d6a88309f57033f600f7837f8e651
Erlantz <erlantzram@gmail.com>
2022-06-29 11:31:08 +0100

    it displays 'it is a draw' when all movements has been done wihtout a winner

--- src/index.js
+++ src/index.js
@@ -101,6 +101,8 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
+    } else if (this.state.stepNumber === 9) {
+      status = 'It is a draw';
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
